ba0614c85057ddeeb08a0d9ffcca08651c82d1c6
BVMarius <marius.moraru@live.com>
2020-11-16 21:15:27 +0200

    created stubbed controller for user api

---
+++ user.management/src/main/java/ro/agilehub/javacourse/car/hire/user/controller/UserController.java
@@ -0,0 +1,65 @@
+package ro.agilehub.javacourse.car.hire.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+import ro.agilehub.javacourse.car.hire.api.model.CreateUserRequest;
+import ro.agilehub.javacourse.car.hire.api.model.SuccessResponse;
+import ro.agilehub.javacourse.car.hire.api.model.UserResponse;
+import ro.agilehub.javacourse.car.hire.api.specification.ApiApi;
+import ro.agilehub.javacourse.car.hire.api.specification.ApiUtil;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+public class UserController implements ApiApi {
+
+
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse> createUser(@Valid CreateUserRequest createUserRequest) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteUser(String userId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<UserResponse> getUserById(Long userId) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(UUID.randomUUID().toString());
+        userResponse.setFirstName("Jhon");
+        return new ResponseEntity<>(userResponse,HttpStatus.valueOf(200));
+    }
+
+    @Override
+    public ResponseEntity<Void> updateUser(String userId, @Valid CreateUserRequest createUserRequest) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<UserResponse>> apiUserListGet() {
+
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(UUID.randomUUID().toString());
+        userResponse.setFirstName("Jhon");
+        List<UserResponse> userResponseList = Collections.singletonList(userResponse);
+
+
+        return new ResponseEntity<>(userResponseList,HttpStatus.valueOf(200));
+    }
+}
